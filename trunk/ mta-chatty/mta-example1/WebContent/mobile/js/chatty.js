6aa17d4f87b264961dd2c40b2c6c50aec3093bd0
josefn.il@gmail.com <josefn.il@gmail.com@f78c43de-d86d-7a75-ee0d-c25d49963b7e>
2013-04-14 19:57:09 +0000

    dynamic buddy selection

--- trunk/ mta-chatty/mta-example1/WebContent/mobile/js/chatty.js
+++ trunk/ mta-chatty/mta-example1/WebContent/mobile/js/chatty.js
@@ -1,21 +1,47 @@
 
 $(document).ready(function(){
 	$("a[data-role=tab]").each(function () {
-		console.log("tab-data role");
 	    var anchor = $(this);
 	    anchor.bind("click", function () {
 	        $.mobile.changePage(anchor.attr("href"), {
 	            transition: "none",
 	            changeHash: false
 	        });
-	    	console.log("tab-data role..");
 	        return false;
 	    });
 	});
 
+	
+	$("a[data-role=buddy]").each(function () {
+	    var anchor = $(this);
+	    anchor.bind("click", function () {
+		      var id = $(this).attr("id");
+		      changePage("#ChatRoom", { "id": id });
+	        return false;
+	    });
+	});
+
+
 	$("div[data-role=page]").bind("pagebeforeshow", function (e, data) {
-		console.log("pagebeforeshow");
 	    $.mobile.silentScroll(0);
 	    $.mobile.changePage.defaults.transition = 'slide';
 	});
+	
+	
+	// bind callback that will be triggered after a pageshow event
+	$("#ChatRoom").bind("callback", function(e, args) {
+		console.log("bind callback: "+args.id);
+		$("#ChatRoom .room-title").text(args.id);
+	});
+	
 });
+
+
+function changePage(page, args) 
+{
+		$.mobile.changePage(page, { changeHash: true });
+        $(page).trigger("callback", args);
+}
+
+
+
