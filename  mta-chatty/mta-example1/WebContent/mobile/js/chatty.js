0534435acb13c6da4cbd9131e2fb270ed08bbd0e
yosizil@gmail.com <yosizil@gmail.com@f78c43de-d86d-7a75-ee0d-c25d49963b7e>
2013-04-15 08:39:18 +0000

    Arranging

---  mta-chatty/mta-example1/WebContent/mobile/js/chatty.js
+++  mta-chatty/mta-example1/WebContent/mobile/js/chatty.js
@@ -12,15 +12,26 @@ $(document).ready(function(){
 	});
 
 	
-	$("a[data-role=buddy]").each(function () {
+	$("a[href=#ChatRoom]").each(function () {
 	    var anchor = $(this);
-	    anchor.bind("click", function () {
+		anchor.bind("click", function () {
 		      var id = $(this).attr("id");
+		      console.log("Going to change Chat room with id: " + id);
 		      changePage("#ChatRoom", { "id": id });
 	        return false;
 	    });
 	});
 
+	/*
+	$("footer[class=messageEnter]").each(function () {
+	    var anchor = $(this);
+	    anchor.bind("click", function () {
+	    	$("messageErea").text("");
+	        });
+	        return false;
+	    });
+	});
+	*/
 
 	$("div[data-role=page]").bind("pagebeforeshow", function (e, data) {
 	    $.mobile.silentScroll(0);
@@ -30,12 +41,40 @@ $(document).ready(function(){
 	
 	// bind callback that will be triggered after a pageshow event
 	$("#ChatRoom").bind("callback", function(e, args) {
-		console.log("bind callback: "+args.id);
-		$("#ChatRoom .room-title").text(args.id);
+		activateRoom(args.id);
 	});
 	
 });
 
+
+function getImgLinkFromID(Id)
+{
+	switch(Id)
+	{
+	case "bart":    return "image/Bart.jpg"    ; break;
+	case "homer":   return "image/Homer.jpg"   ; break;
+	case "maggie":  return "image/Maggie.jpg"  ; break;
+	case "lisa":    return "image/Lisa.jpg"    ; break;
+	case "marge":   return "image/Marge.jpg"   ; break;
+	case "simsons": return "image/Simsons.jpg" ; break;
+	}
+	return "image/PhotoUnavailable.jpg";
+}
+
+function getNameFromID(Id)
+{
+	return Id;
+}
+
+function activateRoom(roomId)
+{
+	var buddyName = getNameFromID(roomId);
+	$("#ChatRoom .room-label").text(buddyName);
+	document.title = buddyName;
+	var buddyImg = getImgLinkFromID(roomId);	
+	console.log("buddyName: "+ buddyName + ", buddyImg: "+ buddyImg);
+	$("#ChatRoom .room-image").attr("src", buddyImg);
+}
 
 function changePage(page, args) 
 {
