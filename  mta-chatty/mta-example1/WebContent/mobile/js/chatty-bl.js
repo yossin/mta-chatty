b8055eac00e8f9e88d249323820c336411db5197
josefn.il@gmail.com <josefn.il@gmail.com@f78c43de-d86d-7a75-ee0d-c25d49963b7e>
2013-04-19 08:10:29 +0000

    add dal, util, bl stubs

---
+++  mta-chatty/mta-example1/WebContent/mobile/js/chatty-bl.js
@@ -0,0 +1,36 @@
+var loggedInUser;
+
+//TODO: implement
+function registerNewUser(user){
+	// validate and register user with server
+}
+
+function loginUser(user){
+	// validate user with server
+	// save in local DB
+}
+
+function logoutUser(){
+	// delete from DB?
+}
+
+function getBuddyList(){
+	//email, name, picture
+}
+
+function getGroupList(){
+	//id, name, picture
+}
+
+function getGroupList(){
+	//id, name, picture
+}
+
+function getGroupMessages(groupId){
+	// sender, ts, message
+}
+
+function getBuddyMessages(buddyId){
+	// sender, ts, message
+}
+
