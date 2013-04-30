67c7a694ec16d13d732fdd2680075a284a4c7ead
josefn.il@gmail.com <josefn.il@gmail.com@f78c43de-d86d-7a75-ee0d-c25d49963b7e>
2013-04-30 20:53:51 +0000

    load contracts

---  mta-chatty/mta-example1/WebContent/mobile/js/chattyPC.js
+++  mta-chatty/mta-example1/WebContent/mobile/js/chattyPC.js
@@ -30,6 +30,77 @@ function UI(){
 	}
 	this.navigate=new Navigate();
 	
+	function Draw(){
+		
+		this.buddiesAndGroups=function(buddies, groups){
+			var listRef="#Dashboard .buddiesAndGroupsList";
+			function deleteItems(type){
+			    $("#Dashboard ."+type).remove();
+			}
+			
+			function appendDivider(name){
+			    var e = $('<li data-role="list-divider"><label class="divider-label">'
+			    		+name+'</label></li>');
+			    $(listRef).append(e);
+			}
+			function appendLI(clsid,id,name,img){
+			    var e = $('<li class="'+clsid+
+			    		'"><a href="#ChatRoomUpdate" id="'+id+
+			    		'"><label class="row-label">'+name+
+			    		'</label><img class="row-image" src="'+img+
+			    		'" /></a>');
+			    $(listRef).append(e);
+			}
+			function appendBuddy(b){
+				appendLI("buddy",b.email,b.name,b.picture);
+			}
+			function appendGroup(g){
+				appendLI("group",g.group_id,g.name,g.picture);
+			}
+			function bind(){
+				
+			}
+			deleteItems("divider-label");
+			deleteItems("buddy");
+			deleteItems("group");
+			
+			appendDivider("Buddies");
+			iterateResults(buddies,appendBuddy);
+			appendDivider("Groups");
+			iterateResults(groups,appendGroup);
+		    $(listRef).listview('refresh');
+		    
+		    //bind();
+
+		};
+		
+		this.buddiesMessages=function(messages){
+			
+		};
+	}
+	this.draw = new Draw();
+	
+	function Bind(){
+		this.buddyClick=function(){
+			$("a[href=#ChatRoomUpdate]").each(function () {
+			    var anchor = $(this);
+				anchor.bind("click", function () {
+					//TODO:continue here...
+					clearChatRoom(); // in case of failure - we don't want to see previews room
+					ui.chatroom.id = $(this).attr("id");
+					ui.chatroom.buddyRoom = (ui.chatroom.id.indexOf("@") != -1);
+					console.log("Going to change Chat room with id: " + ui.chatroom.id);
+					if (ui.chatroom.buddyRoom)
+						activateBuddyRoom(ui.chatroom.id);
+					else
+						activateGroupRoom(ui.chatroom.id);
+			        return true;
+			    });
+			});
+		};
+	}
+	this.bind = new Bind();
+	
 	this.startLogin=function(){
 		recreateDB(ui.navigate.login,ui.messages.error);
 	};
@@ -44,6 +115,19 @@ function UI(){
 
 		bl.loginUser(email, pass, ui.navigate.dashboard, ui.messages.loginError);
 		return true;
+	};
+	
+	this.loadContacts=function(){
+		var buddies=undefined;
+		function getGroupListRes(groups){
+			ui.draw.buddiesAndGroups(buddies, groups);
+		}
+		function getBuddyListRes(res){
+			buddies=res;
+			bl.getGroupList(getGroupListRes, ui.messages.error);
+		}
+		bl.getBuddyList(getBuddyListRes, ui.messages.error);
+		
 	};
 }
 var ui=new UI();
@@ -64,6 +148,7 @@ $(document).ready(function(){
     
 	$("#LoginForm .btnLoginToChatty").click(ui.login);
 
+	$("#Dashboard").bind("pagebeforeshow", ui.loadContacts);
     
 });
 
