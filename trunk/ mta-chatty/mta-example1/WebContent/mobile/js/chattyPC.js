e65e5e1d78fb27d23100382361b0b9a6855c897c
josefn.il@gmail.com <josefn.il@gmail.com@f78c43de-d86d-7a75-ee0d-c25d49963b7e>
2013-04-30 18:27:51 +0000

    login logic

--- trunk/ mta-chatty/mta-example1/WebContent/mobile/js/chattyPC.js
+++ trunk/ mta-chatty/mta-example1/WebContent/mobile/js/chattyPC.js
@@ -1,7 +1,59 @@
 
-$(document).ready(function(){
-    
+function UI(){
+	this.context={buddyRoom:true, id:"name@mail.com"};
+	
+	function Messages(){
+		function printError(msg,e){
+			log.error(e);
+			alert(msg+':'+e);
+		}
+		this.error=function(e){
+			printError('error has occured',e);
+		};
+		this.loginError=function(e){
+			printError('error has occured while login please try again',e);
+		};
 
+	}
+	this.messages=new Messages();
+	
+	function Navigate(){
+		this.login=function(){
+			log.debug("navigate into login page");
+			$.mobile.changePage("#Login");
+		};
+		this.dashboard=function(){
+			log.debug("navigate into dashboard page");
+			$.mobile.changePage("#Dashboard");
+		};
+
+	}
+	this.navigate=new Navigate();
+	
+	this.startLogin=function(){
+		recreateDB(ui.navigate.login,ui.messages.error);
+	};
+	
+	this.startApp=function (){
+		bl.checkUserLoggedIn(ui.navigate.dashboard,ui.startLogin);
+	};
+
+	this.login=function(){
+		var email = $("#LoginForm .loginUserMailInput").val();
+		var pass = $("#LoginForm .loginUserPassInput").val();
+
+		bl.loginUser(email, pass, ui.navigate.dashboard, ui.messages.loginError);
+		return true;
+	};
+}
+var ui=new UI();
+
+
+
+
+$(document).ready(function(){
+	initChatty(ui.startApp, ui.messages.error);
+	
     $(window).resize(function(){
         updateMainDivsHeight();
     });
@@ -10,11 +62,20 @@ $(document).ready(function(){
         updateMainDivsHeight();
     });
     
+	$("#LoginForm .btnLoginToChatty").click(ui.login);
+
+    
 });
 
+
 function updateMainDivsHeight(){
-    docHeight = $(document).height();
-    updateHeightPX = ((docHeight - 55) +"px");
-    $(".divBuddiesAndGroups").css({ height: updateHeightPX });
-    $(".divChatRoom").css({ height: updateHeightPX });
+	   docHeight = $(document).height();
+	   updateDashboardHeight = ((docHeight - 55) +"px");
+	   $(".divBuddiesAndGroups").css({ height: updateDashboardHeight });
+	   $(".divChatRoom").css({ height: updateDashboardHeight });
+	   updateDialogHeight = ((docHeight - 250) +"px");
+	   $(".searchBuddyContent").css({ height: updateDialogHeight });
+	   $(".searchGroupContent").css({ height: updateDialogHeight });    
+	   $(".leaveGroupContent").css({ height: updateDialogHeight });    
 }
+	
