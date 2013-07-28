8cc7f8e332e13a636546a335376a058729ac5ea4
yosizil@gmail.com <yosizil@gmail.com@f78c43de-d86d-7a75-ee0d-c25d49963b7e>
2013-07-28 07:46:31 +0000

    Upload group image, admin data to jsp

---  mta-chatty/chatty/WebContent/js/chatty-bl.js
+++  mta-chatty/chatty/WebContent/js/chatty-bl.js
@@ -6,6 +6,8 @@ function initChatty(onSuccess, onError){
 	}
 }
 
+function dummy(){}
+
 function recreateTestDB(onSuccess, onError){
 	createTestData(onSuccess, onError);
 }
@@ -47,19 +49,16 @@ function BL(onSuccessInit, onError){
 	this.registerNewUser=function(email, name, picture, password, onSuccess, onError){
 		var att = {"email":email, "name":name, "picture":picture, "password":password};	
 		ajaxPost("register-user", att, function(data){
-//			loginUser(email, password, onSuccess, onError);
-		}, onError);
-		
-		if (picture != "")
-		{
-			var uploader = new ss.SimpleUpload({
-				button: '#RegisterForm .btnRegisterSubmit', // HTML element used as upload button 
-				url:  "/chatty/services/upload_file", // URL of server-side upload handler
-				name: picture // Parameter name of the uploaded file
-			});
-		}		
+			if (picture != "")
+			{
+				var uploader = new ss.SimpleUpload({
+					button: '#RegisterForm .btnRegisterSubmit', // HTML element used as upload button 
+					url:  "/chatty/services/upload-buddy-image/" + email, // URL of server-side upload handler
+					name: picture // Parameter name of the uploaded file
+				});
+			}		
+		}, onError);		
 		onSuccess();	
-		//dal.insertUser(email, name, picture, password, onSuccess, onError);
 	};
 
 	this.updateUserProfile=function(todo_add_params, onSuccess, onError){
@@ -67,7 +66,6 @@ function BL(onSuccessInit, onError){
 	};
 
 	this.logoutUser=function(onSuccess){
-		// TODO: delete data from DB?
 		dal.removeLoggedInUserId();
 		onSuccess();
 	};
@@ -92,17 +90,13 @@ function BL(onSuccessInit, onError){
 
 	this.addBuddyToList=function(buddyId, onSuccess, onError){
 		var att = {"owner_email":bl.loggedInUser.email, "buddy_id":buddyId};	
-		ajaxPost("add-buddy", att, function(){
-		}, onError);
+		ajaxPost("add-buddy", att, dummy, onError);
 		refreshUserData(onSuccess, onError);
-		
-//		dal.addBuddy(bl.loggedInUser.email, buddyId, onSuccess, onError);
 	};
 
 	//----------- GROUPS -------------
 
 	this.getGroupList=function(onSuccess, onError){
-		//id, name, picture
 		dal.selectGroupList(bl.loggedInUser.email, onSuccess, onError);
 	};
 
@@ -113,39 +107,33 @@ function BL(onSuccessInit, onError){
 
 	this.createGroup=function(name, picture, description, onSuccess, onError){
 		var att = {"member_email":bl.loggedInUser.email, "name":name, "description":description, "picture":picture};
-		ajaxPost("create-group", att, function(){
+		ajaxPost("create-group", att, function(data){
+			if (picture != "")
+			{
+				var new_group_id = data.groups[0].group_id;
+				var service_name = "/chatty/services/upload-group-image/" + new_group_id;
+				var uploader = new ss.SimpleUpload({
+					button: '#CreateGroup .btnCreateGroup', // HTML element used as upload button 
+					url:    service_name, // URL of server-side upload handler
+					name:   picture // Parameter name of the uploaded file
+				});
+			}	
 		}, onError);
 		
-		if (picture != "")
-		{
-			var uploader = new ss.SimpleUpload({
-				button: '#CreateGroup .btnCreateGroup', // HTML element used as upload button 
-				url:  "/chatty/services/upload_file", // URL of server-side upload handler
-				name: picture // Parameter name of the uploaded file
-			});
-		}		
-
 		refreshUserData(onSuccess, onError);
-					
-//		dal.insertGroup(name, picture, description, function(groupId){
-//			dal.insertGroupMembership(bl.loggedInUser.email, groupId, onSuccess, onError);
-//		}, onError);		
 	};
 
 	this.joinGroup=function(groupId, onSuccess, onError){
 		var att = {"member_email":bl.loggedInUser.email, "group_id":groupId};
-		ajaxPost("join-group", att, function(){
-		}, onError);
+		ajaxPost("join-group", att, dummy, onError);
 		refreshUserData(onSuccess, onError);
-		
-//		dal.insertGroupMembership(bl.loggedInUser.email, groupId, onSuccess, onError);
 	};
 
 	this.leaveGroup=function(groupId, onSuccess, onError){
 		var att = {"member_email":bl.loggedInUser.email, "group_id":groupId};	
-		ajaxPost("leave-group", att, function(){}, onError);
+		ajaxPost("leave-group", att, dummy, onError);
 		dal.deleteGroupMembership(bl.loggedInUser.email, groupId, function(){
-			dal.deleteGroupIfEmpry(groupId, function(){}, onError);
+			dal.deleteGroupIfEmpry(groupId, dummy, onError);
 			onSuccess();
 		}, onError);
 	};
@@ -167,37 +155,16 @@ function BL(onSuccessInit, onError){
 
 	this.sendBuddyMessage=function (buddyId, message, onSuccess, onError){
 		var att = {"sender_id":bl.loggedInUser.email, "receiver_id":buddyId, "message":message};	
-		ajaxPost("buddy-message", att, function(){}, onError);
+		ajaxPost("buddy-message", att, dummy, onError);
 		refreshUserData(onSuccess, onError);
-		
-//		dal.insertBuddyMessage(bl.loggedInUser.email, buddyId, message, onSuccess, onError);
 	};
 
 	this.sendGroupMessage=function (groupId, message, onSuccess, onError){
 		var att = {"sender_id":bl.loggedInUser.email, "receiver_id":groupId, "message":message};	
-		ajaxPost("group-message", att, function(){}, onError);
+		ajaxPost("group-message", att, dummy, onError);
 		refreshUserData(onSuccess, onError);
-		
-//		dal.insertGroupMessage(bl.loggedInUser.email, groupId, message, onSuccess, onError);
 	};
 	
-	//----------- ADMIN -------------
-
-	this.getAdminDataSets=function (setAdminPageChartsValues, onError){
-		var dataSetBudies   = []; 
-		var dataSetGroups   = []; 
-		var dataSetMessages = []; 
-		
-		var start = 1354586000000;
-		for (var i = 0; i < 10; i += 0.5){
-			dataSetBudies.push([start, Math.tan(i)]); 
-			dataSetGroups.push([start, Math.sin(i)]); 
-			dataSetMessages.push([start, Math.cos(i)]); 
-	        start+= 100000;
-			}
-		
-		setAdminPageChartsValues(dataSetBudies, dataSetGroups, dataSetMessages);
-	};
 }
 
 
